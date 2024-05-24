912ba6a45abc27987822f2657f71cfbc9092837a
hugo1379 <hugo1379y@gmail.com>
2024-05-24 12:35:49 -0300

    :test: test: get session: ausar banco de dados em memoria

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
@@ -48,18 +50,16 @@ def test_create_user_email_errado_deve_retornar_422(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -76,7 +76,7 @@ def test_update_user(client):
     }
 
 
-def test_update_deve_retornar_404(client):
+def test_update_deve_retornar_404(client, user):
     response = client.put(
         '/users/10',
         json={
@@ -89,7 +89,7 @@ def test_update_deve_retornar_404(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
@@ -102,19 +102,12 @@ def test_delete_deve_retornar_404(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_read_user_deve_retornar_200(client):
-    client.post(
-        '/users/',
-        json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
-        },
-    )
+def test_read_user_deve_retornar_200(client, user):
     response = client.get('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
+        'username': 'Test',
+        'email': 'test@test.com',
         'id': 1,
     }
+
