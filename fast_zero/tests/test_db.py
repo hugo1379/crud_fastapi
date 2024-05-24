56d2d688edcd1403a79379dcfc1d6a2dd6c0fccf
hugo1379 <hugo1379y@gmail.com>
2024-05-24 12:33:08 -0300

    :test: test: get session: recuperacao de sessao

--- fast_zero/tests/test_db.py
+++ fast_zero/tests/test_db.py
@@ -1,13 +1,20 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fast_zero.database import get_session
 from fast_zero.models import User
 
 
 def test_create_user(session):
-    new_user = User(username='alice', password='secret', email='teste@test')
+    new_user = User(username='alice', password='secret', email='test@test')
     session.add(new_user)
     session.commit()
 
     user = session.scalar(select(User).where(User.username == 'alice'))
 
     assert user.username == 'alice'
+
+
+def test_get_session():
+    response = next(get_session())
+    assert isinstance(response, Session)
